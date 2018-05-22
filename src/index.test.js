bbdcb52e0d63110b73e1e6ba5db03091211623c3
kishore <4nm15is025@nmamit.in>
2018-05-22 11:33:44 +0530

    added JSDOM

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('our first test', () => {
 	it('should pass',() => {
 		expect(true).to.equal(true);
 	});
+});
+
+describe('index.html',()=> {
+	it('should passs', ()=>  {
+		const INDEX = fs.readFileSync('src/index.html', "utf-8");
+		jsdom.env(INDEX, function(err, window){
+			const P1 = window.document.getElementsByTagName('p')[0];
+			expect(P1.innerHTML).to.equal("Hello World!");
+			window.close();
+		})
+	})
 })
